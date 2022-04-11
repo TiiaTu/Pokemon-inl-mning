cbb0a60c3ac6d8186e9824d7b858a0096ddad6de
TiiaTu <tiia.tupamaki@gmail.com>
2022-04-11 14:42:45 +0200

    fungerande sökfunktion

--- Pokemon/src/App.jsx
+++ Pokemon/src/App.jsx
@@ -1,47 +1,55 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import { BrowserRouter as Router, NavLink, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Start from './components/Start'
 import PokemonList from './components/PokemonList'
 import Team from './components/Team'
 
+const testData = [
+  {
+    id:1,
+    name: 'Bulbasaur',
+    img: 'https://archives.bulbagarden.net/media/upload/thumb/2/21/001Bulbasaur.png/375px-001Bulbasaur.png'
+  },
+  {
+    id:3,
+    name: 'Ivysaur',
+    img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png'
+  },
+  {
+    id: 4,
+    name: 'Venusaur',
+    img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png'
+  },
+  {
+    id:6,
+    name: 'Charmander',
+    img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'
+  }
+]
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <NavLink to="/start">Start</NavLink>
+            <NavLink to="/pokemons">Pokemons</NavLink>
+            <NavLink to="/my-team">My Team</NavLink>
+          </nav>
+        </header>
+        <main className="App-main">
+          <Routes>
+            <Route path="/" element={<Start />}></Route>
+            <Route path="/pokemons" element={<PokemonList pokemons={testData} />}></Route>
+            <Route path="/my-team" element={<Team />}></Route>
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
--- Pokemon/src/components/PokemonList.jsx
+++ Pokemon/src/components/PokemonList.jsx
@@ -1,7 +1,66 @@
-const PokemonList = () => {
-	return(
-		<div>
+//inspiration till sökbar-funktionen hämtats från: https://blog.logrocket.com/create-search-bar-react-from-scratch/
 
+import { useState } from "react";
+import Team from './Team'
+
+const PokemonList = ({ pokemons }) => {
+	const [query, setQuery] = useState("");
+
+	//sätter input till lower case
+	const inputHandler = (input) => { 
+		let inputAsString = input.target.value.toString();
+		let lowerCased = inputAsString.toLowerCase();	
+		setQuery(lowerCased)
+	}
+
+	//hämtar alla pokemons som matchar söksträngen
+	const filteredList = pokemons.filter(pokemon => {
+		if(query === "") {
+			return true;
+		}
+		else if (pokemon.name.toLowerCase().includes(query)){
+			return true;
+		}
+		else {
+			return false;	
+		}
+	}) 
+
+	//lägger till Pokemon i teamet
+	const addToTeam = (pokemon, team) => {
+		let newTeam = [...team, pokemon];
+		return newTeam;
+	}
+
+	//kollar ifall Pokemon redan finns i teamet
+	const isAlreadyInTeam = (team, teamMember) => {
+		if(team.includes(teamMember)){
+			return (
+				<div>{'This pokemon is already in your team!'}</div>
+			)
+		}
+		
+	}
+
+	//tar bort från listan
+	const takeFromList = () => {
+
+	}
+
+	return(
+		<div className="list-container">
+			<h1 className="list-header">List of available Pokemons</h1>
+			<input placeholder="Enter name of the Pokemon" type="text" onChange={inputHandler} />
+			<ul> 
+				{filteredList.map((pokemon) => (
+				<li key={pokemon.id}>
+					<img src={pokemon.img} alt={pokemon.name} />
+					<p>{pokemon.name}</p>
+					<button className="add-to-team-button" onClick={
+						addToTeam(pokemon, team)
+					}>Add to your team</button>
+					</li>))}
+			</ul>
 		</div>
 	)
 }
