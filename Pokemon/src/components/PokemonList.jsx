cc9328f0d5f548d395f718f4551ada0f362db2b7
TiiaTu <tiia.tupamaki@gmail.com>
2022-04-14 11:27:15 +0300

    inget funkar just nu

--- Pokemon/src/components/PokemonList.jsx
+++ Pokemon/src/components/PokemonList.jsx
@@ -3,65 +3,163 @@
 import { useState } from "react";
 import Team from './Team'
 
-const PokemonList = ({ pokemons }) => {
+const PokemonList = ({ pokemons, teamMembers, setTeamMembers}) => {
 	const [query, setQuery] = useState("");
 
 	//sätter input till lower case
-	const inputHandler = (input) => { 
+	const inputHandler = (input) => {
 		let inputAsString = input.target.value.toString();
-		let lowerCased = inputAsString.toLowerCase();	
+		let lowerCased = inputAsString.toLowerCase();
 		setQuery(lowerCased)
 	}
 
-	//hämtar alla pokemons som matchar söksträngen
+	//hämtar alla pokemons som matchar söksträngen 
 	const filteredList = pokemons.filter(pokemon => {
-		if(query === "") {
+		if (query === "") {
 			return true;
 		}
-		else if (pokemon.name.toLowerCase().includes(query)){
+		else if (pokemon.name.toLowerCase().includes(query)) {
 			return true;
 		}
 		else {
-			return false;	
-		}
-	}) 
+			return false;
+		} 
+	})
 
 	//lägger till Pokemon i teamet
-	const addToTeam = (pokemon, team) => {
-		let newTeam = [...team, pokemon];
-		return newTeam;
+	const addToTeam = (pokemon, teamMembers) => {
+		let newTeam = [...teamMembers, pokemon];
+		return setTeamMembers(newTeam);
 	}
 
-	//kollar ifall Pokemon redan finns i teamet
-	const isAlreadyInTeam = (team, teamMember) => {
-		if(team.includes(teamMember)){
-			return (
-				<div>{'This pokemon is already in your team!'}</div>
-			)
-		}
-		
-	}
+	const removeTeamMember = () => {
+		 {
+		const newList = list.filter((item) => item.id !== id);
+	
+		setList(newList);
+	  }
 
-	//tar bort från listan
+	// //kollar ifall Pokemon redan finns i teamet
+	// const isAlreadyInTeam = (Team, newTeamMember) => {
+	// 	if (Team.includes(newTeamMember)) {
+	// 		return (
+	// 			<div>{'This pokemon is already in your team!'}</div>
+	// 		)
+	// 	}
+
+	
+
+	//tar bort Pokemon från listan när man clickar på add to team, vilket även löser problemet med dubbletter
 	const takeFromList = () => {
+		pokemons.filter(pokemon => {
+			if (pokemons.includes(pokemon.name)) {
+				return
+			}
+			else {
+				return true;
+			}
 
+			return updatedList;
+
+		})
 	}
 
-	return(
+	return (
 		<div className="list-container">
 			<h1 className="list-header">List of available Pokemons</h1>
 			<input placeholder="Enter name of the Pokemon" type="text" onChange={inputHandler} />
-			<ul> 
+			<ul>
 				{filteredList.map((pokemon) => (
-				<li key={pokemon.id}>
-					<img src={pokemon.img} alt={pokemon.name} />
-					<p>{pokemon.name}</p>
-					<button className="add-to-team-button" onClick={
-						addToTeam(pokemon, team)
-					}>Add to your team</button>
+					<li key={pokemon.id}>
+						<img src={pokemon.img} alt={pokemon.name} />
+						<p>{pokemon.name}</p>
+						<button className="add-to-team-button" onClick={
+							addToTeam(pokemon, Team)
+						}>Add to team</button>
 					</li>))}
 			</ul>
 		</div>
 	)
 }
+
 export default PokemonList
+
+
+
+
+///OBS!! Denna kod fungerar! Den söker pokemon från listan
+
+
+// const PokemonList = ({ pokemons }) => {
+// 	const [query, setQuery] = useState("");
+// 	const [updatedList, setUpdatedList] = useState()
+
+// 	//sätter input till lower case
+// 	const inputHandler = (input) => {
+// 		let inputAsString = input.target.value.toString();
+// 		let lowerCased = inputAsString.toLowerCase();
+// 		setQuery(lowerCased)
+// 	}
+
+// 	//hämtar alla pokemons som matchar söksträngen
+// 	const filteredList = pokemons.filter(pokemon => {
+// 		if (query === "") {
+// 			return true;
+// 		}
+// 		else if (pokemon.name.toLowerCase().includes(query)) {
+// 			return true;
+// 		}
+// 		else {
+// 			return false;
+// 		}
+// 	})
+
+// 	//lägger till Pokemon i teamet
+// 	const addToTeam = (pokemon, Team) => {
+// 		let newTeam = [...Team, pokemon];
+// 		return newTeam;
+// 	}
+
+// 	//kollar ifall Pokemon redan finns i teamet
+// 	const isAlreadyInTeam = (Team, newTeamMember) => {
+// 		if (Team.includes(newTeamMember)) {
+// 			return (
+// 				<div>{'This pokemon is already in your team!'}</div>
+// 			)
+// 		}
+
+// 	}
+
+// 	//tar bort från listan, vilket även löser problemet med dubbletter
+// 	const takeFromList = () => {
+// 		pokemons.filter(pokemon => {
+// 			if (pokemons.includes(pokemon.name)) {
+// 				return
+// 			}
+// 			else {
+// 				return true;
+// 			}
+
+// 			return updatedList;
+
+// 		})
+// 	}
+
+
+// 	return (
+// 		<div className="list-container">
+// 			<h1 className="list-header">List of available Pokemons</h1>
+// 			<input placeholder="Enter name of the Pokemon" type="text" onChange={inputHandler} />
+// 			<ul>
+// 				{filteredList.map((pokemon) => (
+// 					<li key={pokemon.id}>
+// 						<img src={pokemon.img} alt={pokemon.name} />
+// 						<p>{pokemon.name}</p>
+// 						<button className="add-to-team-button">Add to team</button>
+// 					</li>))}
+// 			</ul>
+// 		</div>
+// 	)
+// }
+
+// export default PokemonList
--- Pokemon/src/components/Team.jsx
+++ Pokemon/src/components/Team.jsx
@@ -1,8 +1,25 @@
-const Team =() => {
-	return(
-		<div>
+import {useState} from './react'
 
+const Team =({pokemons, teamMembers, setTeamMembers}) => {
+	
+	
+	//en funktion som ändrar på namnet av en pokemon, eller ger ett smeknamn till den
+	const changeName = (team, newName) => {
+		let newNickName = {...pokemons, name: newName}
+		return newNickName;
+	}
+
+	//en funktion som tar bort pokemons från listan
+	const kickOutOfTeam = () => {
+
+	}
+
+	return(
+		<div className="team-container">
+			
 		</div>
 	)
 }
 export default Team
+
+//eventuellt lägga till en function som sorterar listan enligt namn, eller ability, eller nyaste?
