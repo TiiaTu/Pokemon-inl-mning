ee449dd1bb4c9c1bf3e44c0fd7ce4eefa8135f76
TiiaTu <tiia.tupamaki@gmail.com>
2022-04-22 16:48:54 +0200

    inlämning

--- Pokemon/src/components/Start.jsx
+++ Pokemon/src/components/Start.jsx
@@ -1,7 +1,15 @@
-const Start = () => {
-	return(
-		<div>
+import {useNavigate} from 'react-router-dom'
 
+const Start = () => {
+	
+	return(
+		<div className="start-page">
+			<>
+			<img className="start-img"src="https://logos-world.net/wp-content/uploads/2020/05/Pokemon-Symbol.jpg" alt="could not load the image" />
+			<h1>Welcome to Pokémon Team Builder!</h1>
+			<p>Build your own Pokémon Team</p>
+			<button onClick={useNavigate}>Get Started</button>		
+			</>
 		</div>
 	)
 }
