dd36a46ffce4bfab4f028ce639ccf1167a8c1db8
TiiaTu <tiia.tupamaki@gmail.com>
2022-04-22 16:48:09 +0200

    PokemonList fungerar inte

--- Pokemon/src/components/PokemonList.jsx
+++ Pokemon/src/components/PokemonList.jsx
@@ -1,10 +1,27 @@
 //inspiration till sökbar-funktionen hämtats från: https://blog.logrocket.com/create-search-bar-react-from-scratch/
 
-import { useState } from "react";
-import Team from './Team'
+import { useEffect, useState } from "react";
 
-const PokemonList = ({ pokemons, teamMembers, setTeamMembers}) => {
+const PokemonList = ({ teamMembers, setTeamMembers }) => {
 	const [query, setQuery] = useState("");
+	const [isFetching, setIsFetching] = useState(false);
+	const [pokemons, setPokemons] = useState([]);
+
+	const fetchData = async () => {
+		setIsFetching(true);
+		const url = `https://pokeapi.co/api/v2/pokemon?limit=151&offset=0`;
+		const response = await fetch(url)
+		const data = await response.json()
+		console.log("Data from fetch:", data)
+		setPokemons(data.results)
+		setIsFetching(false);
+		console.log()
+	}
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
 
 	//sätter input till lower case
 	const inputHandler = (input) => {
@@ -23,143 +40,47 @@ const PokemonList = ({ pokemons, teamMembers, setTeamMembers}) => {
 		}
 		else {
 			return false;
-		} 
+		}
 	})
 
-	//lägger till Pokemon i teamet
-	const addToTeam = (pokemon, teamMembers) => {
-		let newTeam = [...teamMembers, pokemon];
-		return setTeamMembers(newTeam);
+	//lägger till Pokemon i teamet och ger möjlighet att ange ett smeknamn
+	const addToTeam = (pokemon) => {
+		const pokeNickname = window.prompt("Please enter a nickname for your pokémon: ")
+		let newTeamMember = {
+			Pokemon: pokemon,
+			Nickname: pokeNickname,
+		};
+		console.log(newTeamMember);
+
+		setTeamMembers(existingTeam => [...existingTeam, newTeamMember]
+		);
+
+		return (
+			<>
+				<div className="list-container">
+					<h1 className="list-header">List of available Pokémons</h1>
+					<input
+						placeholder="Enter name of the Pokémon" type="text"
+						onChange={inputHandler} />
+						{isFetching === false ? (							
+					<ul>
+						{filteredList.map((pokemon) => (
+							<li key={pokemon.id}>
+								<img src={pokemon.img} alt={pokemon.name} />
+								<p>{pokemon.name}</p>
+								<p>{"#" + pokemon.id}</p>
+								{pokemon.abilities.map((ability) => (
+									<ul key={ability.ability.name}>
+										<li>{"Abilities: " + ability.ability.name}</li>
+									</ul>))}
+								<button 
+								className="add-to-team-button" 
+								onClick={() => addToTeam(pokemon)}>Add to team</button>
+							</li>))}
+					</ul>) : (<p>Loading...</p>)}
+				</div>
+			</>
+		)
 	}
-
-	const removeTeamMember = () => {
-		 {
-		const newList = list.filter((item) => item.id !== id);
-	
-		setList(newList);
-	  }
-
-	// //kollar ifall Pokemon redan finns i teamet
-	// const isAlreadyInTeam = (Team, newTeamMember) => {
-	// 	if (Team.includes(newTeamMember)) {
-	// 		return (
-	// 			<div>{'This pokemon is already in your team!'}</div>
-	// 		)
-	// 	}
-
-	
-
-	//tar bort Pokemon från listan när man clickar på add to team, vilket även löser problemet med dubbletter
-	const takeFromList = () => {
-		pokemons.filter(pokemon => {
-			if (pokemons.includes(pokemon.name)) {
-				return
-			}
-			else {
-				return true;
-			}
-
-			return updatedList;
-
-		})
-	}
-
-	return (
-		<div className="list-container">
-			<h1 className="list-header">List of available Pokemons</h1>
-			<input placeholder="Enter name of the Pokemon" type="text" onChange={inputHandler} />
-			<ul>
-				{filteredList.map((pokemon) => (
-					<li key={pokemon.id}>
-						<img src={pokemon.img} alt={pokemon.name} />
-						<p>{pokemon.name}</p>
-						<button className="add-to-team-button" onClick={
-							addToTeam(pokemon, Team)
-						}>Add to team</button>
-					</li>))}
-			</ul>
-		</div>
-	)
 }
-
-export default PokemonList
-
-
-
-
-///OBS!! Denna kod fungerar! Den söker pokemon från listan
-
-
-// const PokemonList = ({ pokemons }) => {
-// 	const [query, setQuery] = useState("");
-// 	const [updatedList, setUpdatedList] = useState()
-
-// 	//sätter input till lower case
-// 	const inputHandler = (input) => {
-// 		let inputAsString = input.target.value.toString();
-// 		let lowerCased = inputAsString.toLowerCase();
-// 		setQuery(lowerCased)
-// 	}
-
-// 	//hämtar alla pokemons som matchar söksträngen
-// 	const filteredList = pokemons.filter(pokemon => {
-// 		if (query === "") {
-// 			return true;
-// 		}
-// 		else if (pokemon.name.toLowerCase().includes(query)) {
-// 			return true;
-// 		}
-// 		else {
-// 			return false;
-// 		}
-// 	})
-
-// 	//lägger till Pokemon i teamet
-// 	const addToTeam = (pokemon, Team) => {
-// 		let newTeam = [...Team, pokemon];
-// 		return newTeam;
-// 	}
-
-// 	//kollar ifall Pokemon redan finns i teamet
-// 	const isAlreadyInTeam = (Team, newTeamMember) => {
-// 		if (Team.includes(newTeamMember)) {
-// 			return (
-// 				<div>{'This pokemon is already in your team!'}</div>
-// 			)
-// 		}
-
-// 	}
-
-// 	//tar bort från listan, vilket även löser problemet med dubbletter
-// 	const takeFromList = () => {
-// 		pokemons.filter(pokemon => {
-// 			if (pokemons.includes(pokemon.name)) {
-// 				return
-// 			}
-// 			else {
-// 				return true;
-// 			}
-
-// 			return updatedList;
-
-// 		})
-// 	}
-
-
-// 	return (
-// 		<div className="list-container">
-// 			<h1 className="list-header">List of available Pokemons</h1>
-// 			<input placeholder="Enter name of the Pokemon" type="text" onChange={inputHandler} />
-// 			<ul>
-// 				{filteredList.map((pokemon) => (
-// 					<li key={pokemon.id}>
-// 						<img src={pokemon.img} alt={pokemon.name} />
-// 						<p>{pokemon.name}</p>
-// 						<button className="add-to-team-button">Add to team</button>
-// 					</li>))}
-// 			</ul>
-// 		</div>
-// 	)
-// }
-
-// export default PokemonList
+	export default PokemonList
