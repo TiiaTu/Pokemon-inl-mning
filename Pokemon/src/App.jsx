f48d96bf5f0116295ec0c391d6f301465ff3056b
TiiaTu <tiia.tupamaki@gmail.com>
2022-05-23 07:03:21 +0200

    pokelist och team fungerar inte

--- Pokemon/src/App.jsx
+++ Pokemon/src/App.jsx
@@ -9,7 +9,14 @@ import startpage from './css/startpage.css'
 function App() {
   const [teamMembers, setTeamMembers] = useState([])
 
-  
+
+  const addTeamMember = (pokemon) => {
+    let newPokemon = { team_id: '' }
+    newPokemon = { ...pokemon, team_id: 'team' + teamMembers.length }
+    setTeamMembers(oldTeam => [...oldTeam, newPokemon])
+    alert(`You have added ${pokemon.name} to your team!`)
+    return teamMembers
+  }
 
   return (
     <Router>
@@ -17,16 +24,25 @@ function App() {
         <header className="App-header">
           <nav className="navibar">
             <NavLink to="/">Start</NavLink>
-            <NavLink to="/teamMembers">Pokemons</NavLink>
+            <NavLink to="/pokemons">Pokemons</NavLink>
             <NavLink to="/my-team">My Team</NavLink>
           </nav>
         </header>
         <main className="App-main">
-            <Routes>
-              <Route path="/" element={<Start />}></Route>
-              <Route path="/teamMembers" element={<PokemonList teamMembers={teamMembers} setTeamMembers={setTeamMembers} />}></Route>
-              <Route path="/my-team" element={<Team teamMembers={teamMembers} setTeamMembers={setTeamMembers} />}></Route>
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Start />}></Route>
+            <Route path="/pokemons" element={
+              <PokemonList
+                teamMembers={teamMembers}
+                setTeamMembers={setTeamMembers}
+                addTeamMember={addTeamMember} />}>
+            </Route>
+            <Route path="/my-team" element={
+              <Team
+                teamMembers={teamMembers}
+                setTeamMembers={setTeamMembers} />}>
+            </Route>
+          </Routes>
         </main>
       </div>
     </Router>
